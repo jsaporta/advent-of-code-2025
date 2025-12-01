d1472540943f6a3bbf5fefcf2e17920c1a4d8578
Jason Saporta <jasons89@gmail.com>
2025-12-01 06:01:31 -0500

    Day 1, not yet complete

---
+++ day1.hs
@@ -0,0 +1,11 @@
+processItem :: String -> Int
+processItem s
+	| head s == 'L' = -(read $ tail s)
+	| otherwise     = read $ tail s
+
+main :: IO ()
+main = do
+	contents <- fmap lines $ readFile "input"
+	let locations = map processItem contents in
+		let prefixSum = scanl (+) 50 locations in
+			putStrLn $ show (length (filter (\x -> x `mod` 100 == 0) prefixSum))
